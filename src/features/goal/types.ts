399d58f83b1510e1ab53f5d97db9b9a8f6962d6b
Takashi-Otsuka <tukatyu8252@gmail.com>
2026-05-30 18:58:10 +0900

    feat: define domain models (#5)

---
+++ src/features/goal/types.ts
@@ -0,0 +1,8 @@
+export type GoalSetting = {
+  currentWeightKg?: number;
+  startWeightKg?: number;
+  targetWeightKg?: number;
+  startDate?: string;
+  targetDate?: string;
+  heightCm?: number;
+};
